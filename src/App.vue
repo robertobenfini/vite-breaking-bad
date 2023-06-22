23524ebcdf83809fca5c07c8a66118aa994c4833
robertobenfini <roberto.benfini@gmail.com>
2023-06-22 13:31:27 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+export default {
+  components:{
+    AppHeader,
+    AppMain
+  }
+}
+</script>
+
+<template>
+  
+  <div>
+    
+    <AppHeader />
+    <AppMain />
+
+  </div>
+
+</template>
+
+<style lang="scss">
+  @use './styles/generals.scss' as *;
+  @use './styles/partials/variables' as *;
+</style>
